3f05a03cd70408cc229be0167990966b0dd06208
Remi Rampin <remi@rampin.org>
2024-04-07 10:51:21 -0400

    Add build scripts

---
+++ scripts/docker_build_push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+VERSION=$(git describe | sed 's/^v//')
+IMAGE=ghcr.io/remram44/k8s-emailer:$VERSION
+
+docker buildx build --pull \
+    . \
+    --cache-from type=registry,ref=ghcr.io/remram44/k8s-emailer/buildxcache \
+    --cache-to type=registry,mode=max,ref=ghcr.io/remram44/k8s-emailer/buildxcache,oci-mediatypes=false \
+    --platform linux/amd64,linux/arm64 \
+    --push --tag $IMAGE
+
+echo
+echo "    $IMAGE"
